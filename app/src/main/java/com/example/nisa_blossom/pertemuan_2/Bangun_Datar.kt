7b86d91224294d646c024a9636f82be60cd46574
nisapcr <harnisa24@mahasiswa.pcr.ac.id>
2026-03-31 14:26:34 +0700

    first commit

---
+++ app/src/main/java/com/example/nisa_blossom/pertemuan_2/Bangun_Datar.kt
@@ -0,0 +1,12 @@
+package com.example.nisa_blossom.pertemuan_2
+
+class Bangun_Datar {
+
+    fun luasSegitiga(alas: Double, tinggi: Double): Double {
+        return 0.5 * alas * tinggi
+    }
+
+    fun volumeKubus(sisi: Double): Double {
+        return sisi * sisi * sisi
+    }
+}
